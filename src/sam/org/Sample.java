07b7b7c3cd694dd4cfdcdc5cb9f28614a7dbb696
Abdkalam <amjabdul@gmail.com>
2022-05-30 13:22:21 +0530

    overall codes

---
+++ src/sam/org/Sample.java
@@ -0,0 +1,15 @@
+package sam.org;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		String a = "apple";
+		String a1 = "system";
+		System.out.println(System.identityHashCode(a));
+		a = a.concat(a1);
+
+		System.out.println(System.identityHashCode(a));
+	}
+
+}
